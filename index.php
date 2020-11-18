feeca40ca0678d2faa29fb6e5cc555ad92a87fd6
bambalam <bambalam@dubaided.gov.ae>
2020-11-18 21:52:29 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=600, initial-scale=1.0">
+    <meta name="description" content="tutor">
+  <meta name="author" content="baburaj.ambalam@gmail.com">
+  <meta name="copyright" content="Copyright 2020 Baburaj Ambalam (baburaj.ambalam@gmail.com). All rights reserved.">
+    <title>index</title>
+</head>
+<body>
+<a href="trainer.php">trainer</a>
+</body>
+</html>
